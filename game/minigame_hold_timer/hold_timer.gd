acb827654c86306f7f4f90b6cb868c5d1a71c3cb
JakobSchauser <jakob.schauser@gmail.com>
2019-08-03 01:16:47 +0200

    started hold_timer

---
+++ game/minigame_hold_timer/hold_timer.gd
@@ -0,0 +1,31 @@
+extends Sprite
+
+var hold_time = 1.5
+var curr_time = 0
+var threshold = 0.1
+var startpos
+signal won
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	startpos = position
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if(curr_time > hold_time+threshold) or (Input.is_action_just_pressed("ui_down")):
+		curr_time = 0
+	if(Input.is_action_pressed("ui_down")):
+		curr_time += delta
+	if(Input.is_action_just_released("ui_down")):
+		if(abs(curr_time-hold_time)<threshold):
+			emit_signal("won")
+			
+	var p = startpos
+	p.y -= curr_time*200
+	position = p
+	if(abs(curr_time-hold_time)<threshold):
+		print("won")
+		modulate = Color(0,1,0,1)
+	else:
+		print(curr_time)
+		modulate = Color(1,1,1,1)
