b36c66bb8cd4bd40f566df9e12fefc938854939d
JakobSchauser <jakob.schauser@gmail.com>
2019-08-03 14:36:26 +0200

    started channel minigame

---
+++ game/minigame_channel/tv.gd
@@ -0,0 +1,34 @@
+extends Sprite
+
+var correct_channel = 27
+var current_channel = 0
+onready var channel = $channel
+onready var sound = $sound
+onready var label = $Label
+var correct_volume = 7
+var volume = 0
+var done = false
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if(done):
+		return
+	if(Input.is_action_just_pressed("ui_up")):
+		current_channel += 1
+	if(Input.is_action_just_pressed("ui_down")):
+		current_channel -= 1
+	if(Input.is_action_just_pressed("ui_left")):
+		volume -= 1
+	if(Input.is_action_just_pressed("ui_right")):
+		volume += 1
+	volume = min(max(0,volume),10)
+	
+	channel.frame = current_channel
+	sound.volume_db = volume
+	label.text = "volume: " + str(volume) + "\nchannel: " + str(current_channel)
+	
+	if(current_channel == correct_channel and volume == correct_volume):
+		done = true
