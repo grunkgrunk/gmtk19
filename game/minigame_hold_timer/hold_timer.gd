5a24d2c772c0adbe017502e0e794b93902069c0b
JakobSchauser <jakob.schauser@gmail.com>
2019-08-04 16:41:08 +0200

    made covfefe game more understandable

--- game/minigame_hold_timer/hold_timer.gd
+++ game/minigame_hold_timer/hold_timer.gd
@@ -1,4 +1,4 @@
-extends Sprite
+extends AnimatedSprite
 
 signal win
 var hold_time = 1.5
@@ -6,13 +6,24 @@ var curr_time = 0
 var threshold = 0.1
 var startpos
 
+export var canhold = true
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	startpos = position
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if(curr_time > hold_time+threshold) or (Input.is_action_just_pressed("ui_down")):
+	if !canhold:
+		print("no!")
+		return
+	frame = 0
+	if(curr_time > hold_time+threshold):
+		curr_time = 0
+		frame = 2
+		canhold = false
+		$anim.play("new_cup")
+	if(Input.is_action_just_pressed("ui_down")):
 		curr_time = 0
 	if(Input.is_action_pressed("ui_down")):
 		curr_time += delta
@@ -20,11 +31,12 @@ func _process(delta):
 		if(abs(curr_time-hold_time)<threshold):
 			emit_signal("win")
 			
-	var p = startpos
-	p.y -= curr_time*200
-	position = p
+	#var p = startpos
+	#p.y -= curr_time*200
+	#position = p
 	if(abs(curr_time-hold_time)<threshold):
-		modulate = Color(0,1,0,1)
+		#modulate = Color(0,1,0,1)
+		frame = 1
 	else:
-
-		modulate = Color(1,1,1,1)
+		pass
+		#modulate = Color(1,1,1,1)
